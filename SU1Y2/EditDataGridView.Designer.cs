2400efe24e47725f5f902d14352d5133ee97e0cd
rathy2303 <thyjks2020@gmail.com>
2024-01-25 10:20:26 +0700

    No Update

---
+++ SU1Y2/EditDataGridView.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SU1Y2
+{
+    partial class EditDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGV1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGV1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGV1
+            // 
+            this.DataGV1.AllowUserToAddRows = false;
+            this.DataGV1.AllowUserToDeleteRows = false;
+            this.DataGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGV1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.DataGV1.Location = new System.Drawing.Point(36, 281);
+            this.DataGV1.Name = "DataGV1";
+            this.DataGV1.ReadOnly = true;
+            this.DataGV1.RowHeadersWidth = 51;
+            this.DataGV1.RowTemplate.Height = 64;
+            this.DataGV1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DataGV1.Size = new System.Drawing.Size(756, 367);
+            this.DataGV1.TabIndex = 21;
+            this.DataGV1.SelectionChanged += new System.EventHandler(this.DataGV1_SelectionChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "ID";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 190;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Image";
+            this.Column3.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // pic1
+            // 
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic1.Image = global::SU1Y2.Properties.Resources.noimage;
+            this.pic1.Location = new System.Drawing.Point(56, 35);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(132, 155);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 24;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(237, 206);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(276, 38);
+            this.txtName.TabIndex = 23;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(56, 206);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(164, 38);
+            this.txtID.TabIndex = 22;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(237, 93);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(132, 54);
+            this.btnUpdate.TabIndex = 25;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(381, 93);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(132, 54);
+            this.btnBack.TabIndex = 26;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // EditDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 692);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.DataGV1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "EditDataGridView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EditDataGridView";
+            this.Load += new System.EventHandler(this.EditDataGridView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGV1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGV1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewImageColumn Column3;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
---
+++ SU1Y2/EditDataGridView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SU1Y2
+{
+    public partial class EditDataGridView : Form
+    {
+        public EditDataGridView()
+        {
+            InitializeComponent();
+        }
+
+        public EditDataGridView(DataGridViewRowCollection rows,Form THIS)
+        {
+            InitializeComponent();
+            this.rows = rows;
+            this.THIS = THIS;
+            foreach(DataGridViewRow row in rows)
+            {
+                string id = row.Cells[0].Value.ToString();
+                string name = row.Cells[1].Value.ToString();
+                Image img = (Image)row.Cells[2].Value;
+                DataGV1.Rows.Add(id, name, img);
+            }
+        }
+
+        private DataGridViewRowCollection rows;
+        Form THIS;
+
+
+        private void EditDataGridView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGV1_SelectionChanged(object sender, EventArgs e)
+        {
+            if(DataGV1.Rows.Count > 0)
+            {
+                int rowsIndex = DataGV1.SelectedRows[0].Index;
+                DataGridViewRow row = DataGV1.Rows[rowsIndex];
+                txtID.Text = row.Cells[0].Value.ToString();
+                txtName.Text = row.Cells[1].Value.ToString();
+                Image img = (Image)row.Cells[2].Value;
+                pic1.Image = img;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            /* string id = txtID.Text;
+             string name = txtName.Text;
+             Image img = pic1.Image;
+             int rowsIndex = DataGV1.SelectedRows[0].Index;
+             DataGridViewRow row = DataGV1.Rows[rowsIndex];
+             row.Cells[0].Value = id;
+             row.Cells[1].Value = name;
+             row.Cells[2].Value = img;
+
+             txtID.Clear();
+             txtName.Clear();
+             pic1.Image = Properties.Resources.noimage;
+
+             rows[rowsIndex].Cells[0].Value = id;
+             rows[rowsIndex].Cells[1].Value = name;
+             rows[rowsIndex].Cells[2].Value = img;*/
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            THIS.Show();
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            DialogResult dr = of.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                Image img = Image.FromFile(of.FileName);
+                pic1.Image = img;
+            }
+        }
+    }
+}
